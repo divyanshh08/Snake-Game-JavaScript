08a9cd79f166d9d0be9bff41f3c519787449580f
Divyansh Yadav <divyansh.01072007@gmail.com>
2026-07-19 20:56:22 +0530

    Add snake head styling, self-collision detection, and code refactoring

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const blockHeight = 50;
 const blockWidth = 50;
 
 // "||" means if the value of localStorage.getItem("highScore") is null or undefined, then it will assign 0 to highScore. This is a common way to provide a default value in JavaScript.
-let highScore = localStorage.getItem("highScore") || 0; 
+let highScore = localStorage.getItem("highScore") || 0;
 let score = 0;
 let time = `00-00`;
 
@@ -51,7 +51,6 @@ for (let row = 0; row < rows; row++) {
 
 highScoreElement.innerText = highScore;
 
-
 function render() {
   let head = null;
 
@@ -59,35 +58,43 @@ function render() {
   blocks[`${food.x}-${food.y}`].classList.add("food");
 
   // snake arrow movement logic
-  if (direction === "left") {
-    head = { x: snake[0].x, y: snake[0].y - 1 };
-  } else if (direction === "right") {
-    head = { x: snake[0].x, y: snake[0].y + 1 };
-  } else if (direction === "down") {
-    head = { x: snake[0].x + 1, y: snake[0].y };
-  } else if (direction === "up") {
-    head = { x: snake[0].x - 1, y: snake[0].y };
+  switch (direction) {
+    case "left":
+      head = { x: snake[0].x, y: snake[0].y - 1 };
+      break;
+
+    case "right":
+      head = { x: snake[0].x, y: snake[0].y + 1 };
+      break;
+
+    case "down":
+      head = { x: snake[0].x + 1, y: snake[0].y };
+      break;
+
+    case "up":
+      head = { x: snake[0].x - 1, y: snake[0].y };
+      break;
+
+    default:
+      // Optional: handle invalid direction
+      break;
   }
 
   //wall collision logic
   if (head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols) {
-    clearInterval(intervalId); // random food generation, motion, of snake stopped
-    clearInterval(timeIntervalId); // time update stopped
-    modal.style.display = "flex";
-
-    startGameModal.style.display = "none";
-    gameOverModal.style.display = "flex";
+    gameOver();
     return;
   }
+
+  // self collision logic
+  checkSelfCollision(head);
 
   // food consume logic
   if (head.x === food.x && head.y === food.y) {
     blocks[`${food.x}-${food.y}`].classList.remove("food");
 
-    food = {
-      x: Math.floor(Math.random() * rows),
-      y: Math.floor(Math.random() * cols),
-    };
+    generateFood();
+
     blocks[`${food.x}-${food.y}`].classList.add("food");
     snake.unshift(head);
 
@@ -101,35 +108,78 @@ function render() {
     }
   }
 
+  // draw snake logic ( remove classes from previous snake position -> move snake -> draw snake)
+  drawSnake(head);
+}
+
+function generateFood() {
+  food = {
+    x: Math.floor(Math.random() * rows),
+    y: Math.floor(Math.random() * cols),
+  };
+}
+
+function checkSelfCollision(head) {
+  for (let segment of snake) {
+    if (segment.x === head.x && segment.y === head.y) {
+      clearInterval(intervalId);
+      clearInterval(timeIntervalId);
+
+      modal.style.display = "flex";
+      startGameModal.style.display = "none";
+      gameOverModal.style.display = "flex";
+
+      return;
+    }
+  }
+}
+function drawSnake(head) {
   // snake motion logic
   snake.forEach((segment) => {
     blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
+    blocks[`${segment.x}-${segment.y}`].classList.remove("snake_head");
   });
+
+  // draw snake head and body
   snake.unshift(head); // add new head to the snake
   snake.pop(); // remove the tail segment of the snake
   snake.forEach((segment) => {
-    blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+    if (segment.x === head.x && segment.y === head.y) {
+      blocks[`${segment.x}-${segment.y}`].classList.add("snake_head");
+    } else {
+      blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+    }
   });
+}
+
+function startGame() {
+  // snake and food rendering begins.
+  intervalId = setInterval(() => {
+    render();
+  }, 400);
+
+  startTime();
+}
+
+function startTime() {
+  timeIntervalId = setInterval(() => {
+    let [min, sec] = time.split("-").map(Number);
+
+    if (sec === 59) {
+      min++;
+      sec = 0;
+    } else {
+      sec++;
+    }
+
+    time = `${min}-${sec}`;
+    timeElement.innerText = time;
+  }, 1000);
 }
 
 startButton.addEventListener("click", () => {
   modal.style.display = "none";
-  intervalId = setInterval(() => {
-    render();
-  }, 300);
-
-  // setInterval is a built-in JavaScript function that allows you to execute a function repeatedly at specified intervals (in milliseconds).
-  timeIntervalId = setInterval(() => {
-    let [min, sec] = time.split("-").map(Number);
-    if (sec == 59) {
-      min += 1;
-      sec = 0;
-    } else {
-      sec += 1;
-    }
-    time = `${min}-${sec}`;
-    timeElement.innerText = time;
-  }, 1000); // 1000 here means 1 second, so the time will be updated every second and this function will start after 1 second.
+  startGame();
 });
 
 restartButton.addEventListener("click", () => {
@@ -137,14 +187,18 @@ restartButton.addEventListener("click", () => {
 });
 
 function restartGame() {
-  // intervalId is running the render function every 300 milliseconds.
-  clearInterval(intervalId);  // clearInterval is a built-in JavaScript function that stops the execution of a function that was set to run repeatedly using setInterval.
+  // intervalId is running the render function every 400 milliseconds.
+  clearInterval(intervalId); // clearInterval is a built-in JavaScript function that stops the execution of a function that was set to run repeatedly using setInterval.
   // timeIntervalId is running the time update function every 1000 milliseconds (1 second).
   clearInterval(timeIntervalId);
-  blocks[`${food.x}-${food.y}`].classList.remove("food");
-  snake.forEach((segment) => {
-    blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
-  });
+  // Remove all classes from every block
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      blocks[`${row}-${col}`].classList.remove("fill");
+      blocks[`${row}-${col}`].classList.remove("snake_head");
+      blocks[`${row}-${col}`].classList.remove("food");
+    }
+  }
   score = 0;
   time = `00-00`;
   scoreElement.innerText = score;
@@ -154,38 +208,39 @@ function restartGame() {
   modal.style.display = "none";
   snake = [{ x: 1, y: 3 }];
   direction = "right";
-  food = {
-    x: Math.floor(Math.random() * rows),
-    y: Math.floor(Math.random() * cols),
-  };
+  
+  generateFood();
 
-  // snake and food rendering begins.
-  intervalId = setInterval(() => {
-    render();
-  }, 300);
-
-  // timer begins.
-  timeIntervalId = setInterval(() => {
-    let [min, sec] = time.split("-").map(Number);
-    if (sec == 59) {
-      min += 1;
-      sec = 0;
-    } else {
-      sec += 1;
-    }
-    time = `${min}-${sec}`;
-    timeElement.innerText = time;
-  }, 1000);
+  startGame();
 }
+
+function gameOver() {
+  clearInterval(intervalId);
+  clearInterval(timeIntervalId);
+
+  modal.style.display = "flex";
+  startGameModal.style.display = "none";
+  gameOverModal.style.display = "flex";
+}
+
 // arrow key movement logic
+// Stopping snake from moving in the opposite direction to prevent it from colliding with itself.
 addEventListener("keydown", (event) => {
   if (event.key == "ArrowUp") {
-    direction = "up";
+    if (direction !== "down") {
+      direction = "up";
+    }
   } else if (event.key == "ArrowRight") {
-    direction = "right";
+    if (direction !== "left") {
+      direction = "right";
+    }
   } else if (event.key == "ArrowLeft") {
-    direction = "left";
+    if (direction !== "right") {
+      direction = "left";
+    }
   } else if (event.key == "ArrowDown") {
-    direction = "down";
+    if (direction !== "up") {
+      direction = "down";
+    }
   }
 });
